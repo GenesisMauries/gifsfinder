7bffa1b2d77845ee13411de9e71db6666b0a1f25
GenesisMauries <exodo_54@hotmail.com>
2022-04-28 14:07:57 -0500

    @ViewChild

---
+++ src/app/gifs/busqueda/busqueda.component.ts
@@ -0,0 +1,16 @@
+import { Component, ElementRef, ViewChild } from '@angular/core';
+
+@Component({
+  selector: 'app-busqueda',
+  templateUrl: './busqueda.component.html',
+  styles: [
+  ]
+})
+export class BusquedaComponent{
+  @ViewChild("txtBuscar") txtBuscar!:ElementRef<HTMLInputElement> //:? not null assertion operation 
+
+ buscar(): void{
+    console.log(this.txtBuscar.nativeElement.value);
+    this.txtBuscar.nativeElement.value = ""
+ }
+}
